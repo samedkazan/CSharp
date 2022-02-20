bcf26fa91266e3d2e685da23115790de22c64cf7
Samed <sametkazan92@gmail.com>
2022-02-20 14:00:20 +0300

    Array Class Methods

---
+++ Tutorials/diziler-aray-sınıfı-metodları/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sort
+            int[] sayiDizisi = { 23, 12, 4, 86, 72, 3, 11, 17 };
+            Console.WriteLine("**********SIRASIZ DİZİ ******");
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+            Console.WriteLine("**********SIRALI DİZİ ******");
+            Array.Sort(sayiDizisi);
+
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+            //Clear
+            Console.WriteLine("********** Array Clear ******");
+            // sayiDizisi elemanlarını kullanarak ikinci indeksten itibaren iki tane elemanı sıfırlar
+            Array.Clear(sayiDizisi, 2, 2);
+
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+            // Reverse
+            Console.WriteLine("********** Array Reverse ******");
+
+            Array.Reverse(sayiDizisi);
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+            // İndexOf
+            Console.WriteLine("********** Array İndexOf ******");
+            Console.WriteLine(Array.IndexOf(sayiDizisi, 23));
+
+            //Resize
+            Console.WriteLine("********** Array Resize ******");
+            Array.Resize<int>(ref sayiDizisi, 9);
+            sayiDizisi[8] = 99;
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+
+        }
+    }
+}
