f2127856de4a2c9055fe0fb0930f94752d603ceb
Samed <sametkazan92@gmail.com>
2022-02-20 13:59:35 +0300

    Methods Overloading

---
+++ Tutorials/metotlar_overloading/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // out parametreler
+            string sayi = "999";
+            bool sonuc = int.TryParse(sayi, out int outSayi);
+            if (sonuc)
+            {
+                Console.WriteLine("Başarılı");
+                Console.WriteLine(outSayi);
+            }
+            else
+            {
+                Console.WriteLine("Başarısız");
+            }
+            Metotlar instance = new Metotlar();
+            instance.Topla(4, 5, out int toplamSonucu);
+            Console.WriteLine(toplamSonucu);
+            //Metot Aşırı Yükleme -Overloading
+
+            int ifade = 999;
+            instance.EkranaYazdir(Convert.ToString(ifade));
+            instance.EkranaYazdir(ifade);
+            instance.EkranaYazdir("Samed", "Kazan");
+
+            // Metot İmzası
+            // metot adı + parametre sayısı + parametre tipi
+        }
+    }
+    class Metotlar
+    {
+        public void Topla(int a, int b, out int Toplam)
+        {
+            Toplam = a + b;
+        }
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public void EkranaYazdir(int veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public void EkranaYazdir(string veri1, string veri2)
+        {
+            Console.WriteLine(veri1 + " " + veri2);
+        }
+    }
+
+}
