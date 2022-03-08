230fde35445588d801312d214bc69cdae8b41e05
Samed <sametkazan92@gmail.com>
2022-03-08 20:18:53 +0300

    Enum

---
+++ Tutorials/Enum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine(Gunler.Pazar);
+           Console.WriteLine((int)Gunler.Cumartesi);
+
+           int sıcaklık = 32;
+           if(sıcaklık<=(int)HavaDurumu.Normal)
+            Console.WriteLine("Dışarıya çıkmak için havanın biraz daha ısınmasını bekle...");
+           else if(sıcaklık>=(int)HavaDurumu.Sıcak)
+               Console.WriteLine("Dışarıya çıkmak için çok sıcak");
+            else if(sıcaklık>=(int)HavaDurumu.Normal && sıcaklık<(int)HavaDurumu.Sıcak)
+                Console.WriteLine("hadi dışarıya çıkalım");
+            
+        }
+    }
+    enum Gunler
+    {
+        Pazartesi=1,
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma,
+        Cumartesi,
+        Pazar
+    }
+    enum HavaDurumu
+    {
+        Soguk = 5,
+        Normal = 20,
+        Sıcak = 25,
+        CokSıcak = 30
+    }
+}
